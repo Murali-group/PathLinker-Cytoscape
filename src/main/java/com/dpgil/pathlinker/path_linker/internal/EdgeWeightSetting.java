b2f15927c58747cda24204940a39ae5b907a07a9
Li Huang <lhuang210@gmail.com>
2017-07-13 17:03:00 -0400

    Refs #25: Adds EdgeWeightSetting enum class so both panel and model can access the edge weight setting

---
+++ src/main/java/com/dpgil/pathlinker/path_linker/internal/EdgeWeightSetting.java
@@ -0,0 +1,12 @@
+package com.dpgil.pathlinker.path_linker.internal;
+
+/**
+ * Setting of the edge weight selected by the user
+ */
+public enum EdgeWeightSetting {
+	
+	/**
+	 * Possible edge weight settings
+	 */
+	UNWEIGHTED, ADDITIVE, PROBABILITIES
+}
