4fcb17efdf0b3ca8d16c5cdd7ad2b6c56b56961d
Li Huang <lhuang210@gmail.com>
2017-07-14 15:21:23 -0400

    Refs #22: Add junit maven depdency, creates junit test dir, and creates PathLinkerModelTest class

---
+++ src/test/java/com/dpgil/pathlinker/path_linker/internal/PathLinkerModelTest.java
@@ -0,0 +1,19 @@
+package com.dpgil.pathlinker.path_linker.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PathLinkerModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
