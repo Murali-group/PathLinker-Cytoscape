26212860705b5812c7f932b547b4f9ef2f26d984
Li <lhuang210@gmail.com>
2017-11-19 12:17:23 -0500

    Refs #37: Modify python script and test.txt for testing purposes

--- python_integration_test.py
+++ python_integration_test.py
@@ -6,12 +6,12 @@ from py2cytoscape.data.cyrest_client import CyRestClient
 cy = CyRestClient()
 
 # create a new network from the input txt file
-test_network_input = pd.read_csv('src/test/resources/input/test.txt', header=None, delimiter=r"\s+")
+test_network_input = pd.read_csv('src/test/resources/input/graph-undir_human-interactome.txt', header=None, sep='\t', lineterminator='\n')
 
 source = test_network_input.columns[0]
 target = test_network_input.columns[1]
-edge = test_network_input.columns[2]
-interaction = test_network_input.columns[3]
+interaction = test_network_input.columns[2]
 title = "test run"
 
 test_network = cy.network.create_from_dataframe(test_network_input, source_col=source, target_col=target, interaction_col=interaction, name=title)
+# test_network['interaction'] = test_network['interaction'].astype('float64')
